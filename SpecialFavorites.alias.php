b50b2d34f91f80364b65ae6f756bde72e3b6a611
Jkmartindale <jkmartindale@live.com>
2016-10-18 15:15:05 +0000

    Incorporate Tim Koester's German translation
    Tim Koester provided a nearly complete de.json file on the talk page for the extension. I did my best to correct the few remaining English strings using his translations, so if there's something wrong it's probably my fault.
    
    Change-Id: I762257975d0fb0dacaa914a4e47a88cc641f3db5

--- SpecialFavorites.alias.php
+++ SpecialFavorites.alias.php
@@ -14,4 +14,10 @@ $specialPageAliases = array();
 $specialPageAliases['en'] = array(
 	'Favoritelist' => array( 'Favoritelist', 'My Favorites' ),
 );
- 
+
+/** German
+ * @author Tim Koester
+ */
+$specialPageAliases['de'] = array(
+	'Favoritelist' => array( 'FavoritenListe', 'Favoriten' ),
+);
