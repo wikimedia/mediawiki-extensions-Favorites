5c28da8ca74be4b0c61556ece05c5f7075d3f7e0
zoranzoki21 <zorandori4444@gmail.com>
2021-08-24 07:25:58 +0200

    [includes/FavoriteAction.php] Add param docs
    Change-Id: I93302e9407e706aa38028c3f2fa5907372bd2bbd

--- includes/FavoriteAction.php
+++ includes/FavoriteAction.php
@@ -8,10 +8,20 @@ class FavoriteAction extends BaseAction {
 		return 'favorite';
 	}
 
+	/**
+	 * @inheritDoc
+	 */
 	protected function successMessage() {
 		return 'addedfavoritetext';
 	}
 
+	/**
+	 * @param \Wikimedia\Rdbms\DBConnRef $dbw
+	 * @param int $subject
+	 * @param User $user
+	 * @param Title $title
+	 * @return bool
+	 */
 	protected function doAction(
 		\Wikimedia\Rdbms\DBConnRef $dbw, int $subject, User $user, Title $title
 	) {
