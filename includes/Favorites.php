77f1e5f292930669579cc15df95506a86ef9ba47
Umherirrender <umherirrender_de.wp@web.de>
2025-12-27 19:26:54 +0100

    Replace removed Xml::submitButton/Xml::check
    Also remove a comment mention this function
    
    Bug: T341775
    Follow-Up: Ie5915cf31b675a9755147ed726b2bc7b74b82b94
    Change-Id: I9499cfcf2716fe38b0dc429971656acb3b884a91
    (cherry picked from commit c8bdd86d9175522e9ca58432d27ad38174b56445)

--- includes/Favorites.php
+++ includes/Favorites.php
@@ -45,20 +45,6 @@ class Favorites {
 
 		$token = $this->getFavoriteToken( $title, $user, $mode );
 
-		// from streams:
-		// $fields .= Xml::input ( 'touserid', false, false, array (
-		// 'type' => 'text',
-		// 'id' => 'touserid',
-		// 'placeholder' => wfMessage ( 'streams-push-placeholder' )->plain ()
-		// ) );
-		// $fields .= "<a href='javascript:streamsPush.check_empty()' id='submit' >" . wfMessage ( 'streams-push-send' )->plain () . "</a>";
-		// $formAttribs = array (
-		// 'action' => 'javascript:streamsPush.check_empty();',
-		// 'id' => 'form',
-		// 'method' => 'post',
-		// 'name' => 'pushform'
-		// );
-
 		$links[$place][$mode] = [
 			'class' => $class,
 			'text' => $text,
